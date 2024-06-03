ccfd9ad66460b5b8208b4084782a02cbd0e77d9f
KidNobody <kidnobodyz@bupt.edu.cn>
2024-06-03 11:28:25 +0800

    update files

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 batch_size=32
 epochs=200
+device=0
 learning_rates=(0.1, 0.03, 0.01, 0.003, 0.001)
 weight_decays=(0, 0.0001, 0.0003, 0.001, 0.003, 0.01)
 init_stds=(-1.0, 0.01, 0.03, 0.1, 0.3, 1.0)
 optimizers=("SGD", "Adam")
-device=0
 dataset=("cifar10", "cifar100")
 models=("VGG11", "VGG13", "VGG16", "VGG19", "ResNet18", "ResNet34", "ResNet50", "ResNet101", "ResNet152")
 # shellcheck disable=SC2068
@@ -14,8 +14,9 @@ for lr in ${learning_rates[@]}; do
             for opt in ${optimizers[@]}; do
                 for ds in ${dataset[@]}; do
                     for model in ${models[@]}; do
-                        python train.py --batch_size $batch_size --epochs $epochs --learning_rate $lr --weight_decay $wd --init_std $is --optimizer $opt --device $device --dataset $ds --model $model
+                        python train.py --batch_size $batch_size --epochs $epochs --learning_rate $lr --weight_decay $wd --init_std $is --optimizer $opt --device $device --dataset $ds --model $model &
                     done
+                    wait
                 done
             done
         done
